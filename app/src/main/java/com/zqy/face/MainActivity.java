b00d28ef4a720badc60006bb29ca7f092cd04519
zengqiyan <1214443162@qq.com>
2016-12-13 17:45:58 +0800

    Initial commit

---
+++ app/src/main/java/com/zqy/face/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zqy.face;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture);
+    }
+}
